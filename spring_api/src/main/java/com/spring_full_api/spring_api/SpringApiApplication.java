9de74cf8fdddf607d0b1124527c3eebbcc97b2f2
Flavio Vieira <flavioemerson1@outlook.com>
2022-01-22 15:03:49 -0300

    criado instancia automatica para o banco h2

--- spring_api/src/main/java/com/spring_full_api/spring_api/SpringApiApplication.java
+++ spring_api/src/main/java/com/spring_full_api/spring_api/SpringApiApplication.java
@@ -1,13 +1,32 @@
 package com.spring_full_api.spring_api;
 
+import java.util.Arrays;
+
+import com.spring_full_api.spring_api.Domain.Categoria;
+import com.spring_full_api.spring_api.repository.CategoriaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringApiApplication {
+public class SpringApiApplication implements CommandLineRunner{
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
