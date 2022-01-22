fe0e34f2bd70ee6bdc5002cf0cc71da0f77007d6
Flavio Vieira <flavioemerson1@outlook.com>
2022-01-22 15:30:30 -0300

    criado relaçao entre a table produto e categoria, asossiaçao muitos para muitos

--- spring_api/src/main/java/com/spring_full_api/spring_api/SpringApiApplication.java
+++ spring_api/src/main/java/com/spring_full_api/spring_api/SpringApiApplication.java
@@ -3,7 +3,9 @@ package com.spring_full_api.spring_api;
 import java.util.Arrays;
 
 import com.spring_full_api.spring_api.Domain.Categoria;
+import com.spring_full_api.spring_api.Domain.Produto;
 import com.spring_full_api.spring_api.repository.CategoriaRepository;
+import com.spring_full_api.spring_api.repository.ProdutoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,6 +16,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringApiApplication implements CommandLineRunner{
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringApiApplication.class, args);
@@ -25,8 +29,19 @@ public class SpringApiApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 
+		Produto p1 = new Produto(null, "computador", 2000.00);
+		Produto p2 = new Produto(null, "impressora", 800.00);
+		Produto p3 = new Produto(null, "mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
---
+++ spring_api/src/main/java/com/spring_full_api/spring_api/repository/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.spring_full_api.spring_api.repository;
+
+import com.spring_full_api.spring_api.Domain.Produto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+    
+}
