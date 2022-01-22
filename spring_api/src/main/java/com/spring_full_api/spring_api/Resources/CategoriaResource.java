ff8bfdf3a66447c74fcbfa166b109e461c00fb87
Flavio Vieira <flavioemerson1@outlook.com>
2022-01-22 13:00:25 -0300

    feito metodo get retornar uma lista em memoria

--- spring_api/src/main/java/com/spring_full_api/spring_api/Resources/CategoriaResource.java
+++ spring_api/src/main/java/com/spring_full_api/spring_api/Resources/CategoriaResource.java
@@ -1,5 +1,10 @@
 package com.spring_full_api.spring_api.Resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.spring_full_api.spring_api.Domain.Categoria;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 
     @GetMapping
-    public String listar (){
-        return "Rest funcionando";
+    public List<Categoria> listar (){
+
+        Categoria cat1 = new Categoria(1, "informática");
+        Categoria cat2 = new Categoria(2, "escritorio");
+
+        List<Categoria> lista = new ArrayList<Categoria>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 }
