49aad88eb56de811d7a50af286349a70853efb79
Flavio Vieira <flavioemerson1@outlook.com>
2022-01-22 12:23:24 -0300

    primeiro commit

---
+++ spring_api/src/test/java/com/spring_full_api/spring_api/SpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_full_api.spring_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
