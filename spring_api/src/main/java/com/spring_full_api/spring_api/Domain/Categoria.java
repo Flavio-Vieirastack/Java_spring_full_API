75cf5168387e43cb4c86c416104e4cb012d3ad83
Flavio Vieira <flavioemerson1@outlook.com>
2022-01-22 13:18:05 -0300

    feito primeira migration da entidade categoria ao banco h2

--- spring_api/src/main/java/com/spring_full_api/spring_api/Domain/Categoria.java
+++ spring_api/src/main/java/com/spring_full_api/spring_api/Domain/Categoria.java
@@ -2,9 +2,15 @@ package com.spring_full_api.spring_api.Domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Categoria implements Serializable{
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
