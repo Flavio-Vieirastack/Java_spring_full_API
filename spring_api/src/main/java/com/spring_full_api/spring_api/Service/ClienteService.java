b98980a88bc852754578fffb8a15043c6827d0ac
Flavio Vieira <flavioemerson1@outlook.com>
2022-01-23 11:52:58 -0300

    feito metodo get por 1d para cliente

---
+++ spring_api/src/main/java/com/spring_full_api/spring_api/Service/ClienteService.java
@@ -0,0 +1,23 @@
+package com.spring_full_api.spring_api.Service;
+
+import java.util.Optional;
+
+import com.spring_full_api.spring_api.Domain.Cliente;
+import com.spring_full_api.spring_api.Service.exceptions.ObjectNotFoundException;
+import com.spring_full_api.spring_api.repository.ClienteRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repo;
+
+    public Cliente buscar (Integer id){
+        Optional<Cliente> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+            "Objeto não encontrado, Id: " + id + ", Tipo: " + Cliente.class.getName()));
+    }
+}
