9db6a8d3963c8d3a90a08c4460a4b55d64ad3d8e
Flavio Vieira <flavioemerson1@outlook.com>
2022-01-24 17:38:22 -0300

    feito metodo post para categorias

--- spring_api/src/main/java/com/spring_full_api/spring_api/Resources/CategoriaResource.java
+++ spring_api/src/main/java/com/spring_full_api/spring_api/Resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.spring_full_api.spring_api.Resources;
 
 
+import java.net.URI;
+
 import com.spring_full_api.spring_api.Domain.Categoria;
 import com.spring_full_api.spring_api.Service.CategoriaService;
 
@@ -8,8 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -26,5 +31,16 @@ public class CategoriaResource {
         return ResponseEntity.ok().body(obj);
 
         
+    }
+    @PostMapping
+    public ResponseEntity<Void> insert (@RequestBody Categoria obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder
+        .fromCurrentRequest()
+        .path("/{id}")
+        .buildAndExpand(obj.getId())
+        .toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 }
