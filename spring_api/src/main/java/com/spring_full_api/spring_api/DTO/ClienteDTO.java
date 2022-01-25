44dbb1804bfa07859735d5c0818a2f0a5069b1b9
Flavio Vieira <flavioemerson1@outlook.com>
2022-01-25 15:31:41 -0300

    adicionado put delete e get para cliente

---
+++ spring_api/src/main/java/com/spring_full_api/spring_api/DTO/ClienteDTO.java
@@ -0,0 +1,48 @@
+package com.spring_full_api.spring_api.DTO;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import com.spring_full_api.spring_api.Domain.Cliente;
+
+import org.hibernate.validator.constraints.Length;
+
+
+public class ClienteDTO implements Serializable{
+    private Integer id;
+    @NotEmpty(message = "Preenchimento obrigatório")
+    @Length(min = 5, max = 120, message = "O nome deve conter entre 5 e 120 caractéres")
+    private String nome;
+    @NotEmpty(message = "Preenchimento obrigatório")
+    @Email(message = "Email inválido")
+    private String email;
+    public ClienteDTO() {
+    }
+
+    public ClienteDTO(Cliente obj){
+       id = obj.getId();
+       nome = obj.getNome();
+       email = obj.getEmail(); 
+    }
+    
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
