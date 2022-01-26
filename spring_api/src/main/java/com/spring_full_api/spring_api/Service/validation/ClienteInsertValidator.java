085b01dbeb991ff11c6381ffadf6b052826b874e
Flavio Vieira <flavioemerson1@outlook.com>
2022-01-26 13:24:33 -0300

    feito validaçao customizada para email repetido

--- spring_api/src/main/java/com/spring_full_api/spring_api/Service/validation/ClienteInsertValidator.java
+++ spring_api/src/main/java/com/spring_full_api/spring_api/Service/validation/ClienteInsertValidator.java
@@ -7,14 +7,22 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import com.spring_full_api.spring_api.DTO.ClienteNewDTO;
+import com.spring_full_api.spring_api.Domain.Cliente;
 import com.spring_full_api.spring_api.Domain.enums.TipoCliente;
 import com.spring_full_api.spring_api.Resources.exceptions.FieldMessage;
 import com.spring_full_api.spring_api.Service.validation.utils.BR;
+import com.spring_full_api.spring_api.repository.ClienteRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
     @Override
     public void initialize(ClienteInsert ann) {
     }
+
+    @Autowired
+    ClienteRepository clienteRepository;
 
     @Override
     public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
@@ -25,6 +33,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
         }
         if (objDto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCode()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+        }
+
+        Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+        if (aux != null) {
+            list.add(new FieldMessage("Email", "email existente"));
         }
 
         for (FieldMessage e : list) {
--- spring_api/src/main/java/com/spring_full_api/spring_api/repository/ClienteRepository.java
+++ spring_api/src/main/java/com/spring_full_api/spring_api/repository/ClienteRepository.java
@@ -1,11 +1,14 @@
 package com.spring_full_api.spring_api.repository;
 
+
 import com.spring_full_api.spring_api.Domain.Cliente;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-    
+    @Transactional(readOnly = true)
+    Cliente findByEmail(String email);
 }
